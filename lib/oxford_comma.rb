9a29a4fa3f33d52041ee898aa036258fe8075c4b
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-18 22:57:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,8 @@ def oxford_comma(array)
    array[0]
  elsif array.size == 2
    array.join(" and ")
- elsif array.size == 3
-   array[1].join(", ")
-   array[1-2].join(", and ")
+ elsif array.size >= 3
+   array[-1] = "and #{array[-1]}"
+   array.join(", ")
  end
 end
